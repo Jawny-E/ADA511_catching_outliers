190952416f863b23f0c8366aef94f30c3346e71e
Jawny-E <egrov.johnsen@gmail.com>
2024-11-26 13:45:00 +0100

    Added graphing method for base values

--- project/outliers.py
+++ project/outliers.py
@@ -1,3 +1,6 @@
+# This file plots
+# the distribution of total test-scores
+# and number of correct answers per question
 import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
@@ -8,12 +11,39 @@ df1= pd.read_csv(path1, index_col=0)
 df2 = pd.read_csv(path2, index_col=0)
 
 
-# Some example data to display
-x = np.linspace(0, 2 * np.pi, 400)
-y = np.sin(x ** 2)
-fig, axs = plt.subplots(1, 2)
-fig.suptitle('Vertically stacked subplots')
-axs[0].hist(pd['S1'])
+## Find data from the players
+# Selecting columns after Status and Skill
+successes = df1.iloc[:,2:]
+total_scoresS = np.zeros([len(successes.columns)], dtype=int)
+for index, row in successes.iterrows():
+    print(row.sum())
+    total_scoresS[index] = row.sum()
+print(total_scoresS)
+
+## Find data from the questions
+successes = df2.iloc[:,1:]
+total_scoresQ = np.zeros([len(successes.columns)], dtype=int)
+for index, row in successes.iterrows():
+    print(row.sum())
+    total_scoresQ[index] = row.sum()
+print(total_scoresQ)
+
+# Plotting results
+fig, axs = plt.subplots(1, 2, figsize=(10,7), tight_layout=True)
+fig.suptitle('Plots')
+
+# Student Scores
+num_bins = (total_scoresS.max() - total_scoresS.min())
+axs[0].hist(total_scoresS, num_bins, density=1, color='green', alpha=0.7)
+axs[0].set_xlabel('Student testscores total')
+axs[0].set_ylabel('Amount of students [%]')
+
+# Question Scores
+num_bins = (total_scoresQ.max() - total_scoresQ.min())
+axs[1].bar(df2.index, total_scoresQ, color='blue')
+axs[1].set_xlabel('Question')
+axs[1].set_ylabel('Amount of correct answers')
+
 
 #axs[1].plot(x, -y)
 plt.show()
