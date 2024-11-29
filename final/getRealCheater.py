e98eb58bebdbe9c4069d330c53d0d5f3d1d4b9b4
Jawny-E <egrov.johnsen@gmail.com>
2024-11-29 11:08:05 +0100

    Update column name

--- final/getRealCheater.py
+++ final/getRealCheater.py
@@ -4,5 +4,5 @@ import pandas as pd
 path = "datasets/sample1.csv"
 df= pd.read_csv(path, index_col=0)
 
-row_index = df.index[df['Status'] == 'c'].tolist()
+row_index = df.index[df['Truthfulness'] == 'c'].tolist()
 print(f"The actual cheater was found at index: {row_index}")
